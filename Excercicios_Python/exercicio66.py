32c24924de6fb97b7a7404000c6ff5d22a9b5983
italo-mgl <thenewera07@gmail.com>
2023-10-01 19:00:33 -0300

    corrigindo ex66

--- Excercicios_Python/exercicio66.py
+++ Excercicios_Python/exercicio66.py
@@ -1,13 +1,12 @@
 # Crie um programa que leia vários números inteiros pelo teclado. O programa só vai parar quando o usuário digitar o valor 999
 # No final, mostre quantos números foram digitados e qual foi a soma entre elas.
 
-c = n = s = 0
+c = n  = 0
 while True:
     n = int(input("Digite um numero: "))
     if n == 999:
         c += 1
         break
-    s += n
     c +=1
 
-print(f"A soma dos numerod digitados foi de {s} e voce tentou {c} vezes.")
+print(f"voce tentou {c} vezes.")
