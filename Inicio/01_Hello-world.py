8a373f44af8a7900375b192354cc84e797aaa877
Itin <thenewera07@gmail.com>
2025-06-17 22:27:53 -0300

    inicio novo curso

---
+++ Inicio/01_Hello-world.py
@@ -0,0 +1,16 @@
+#%%
+print("Olá Mundo!!")
+#%%
+
+# Divisão pegando apenas o inteiro
+print("10 // 3 = ", 10 // 3)
+
+#%%
+
+# divisão pegando o resto da divisão
+print("10 / 4 = ", 10 % 4)
+# %%
+
+#potenciação
+print("3 * 3 * 3 = ", 3**3)
+# %%
---
+++ Inicio/02_variaveis.py
@@ -0,0 +1,20 @@
+#%%
+Nome = "Itin"
+
+print(Nome)
+# %%
+Nome = "Garrafa de Água"
+
+print(Nome)
+# %%
+Resultado = 10 * 2
+
+print(Resultado)
+
+Resultado = 30 + 45
+
+print(Resultado)
+
+Resultado = Resultado + 100
+
+print(Resultado)
