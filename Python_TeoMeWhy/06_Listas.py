f2ef42ebc8895defd8c972cdeea45b43ea4f36a1
itin <thenewera07@gmail.com>
2025-07-28 14:07:40 -0300

    listas

---
+++ Python_TeoMeWhy/06_Listas.py
@@ -0,0 +1,41 @@
+idades = [28, 42, 36, 64, 19, 29]
+
+print(idades)
+
+caracteristicas = ["Teo", 32, "Casado", 2341.98]
+
+print(caracteristicas)
+
+type(caracteristicas)
+
+print(caracteristicas[2])
+
+print(caracteristicas[0])
+
+###
+
+idades = [28, 42, 36, 64, 19, 29]
+
+print("Soma idades:", sum(idades))
+
+print(len(idades))
+## Media
+
+print("Media da lista: ", sum(idades) /len(idades))
+
+## menor e maior valor
+
+print("Menor idade:", min(idades))
+print("Maior idade:", max(idades))
+
+
+caracteristicas2 = [
+    "Teo Calvo",
+    32, True, "Casado",
+    ["Estagiario", "Junior", "Pleno", "Senior"],
+    ["Ana", "Maria","Claudia"]
+    ]
+
+print(caracteristicas2[len(caracteristicas2)-1][0])
+
+exs = caracteristicas2[]
---
+++ variados/functions.py
@@ -0,0 +1,21 @@
+
+
+idade = int(input("Ola!!, por gentileza me diga em que ano voce nasceu?"\
+      "Para que assim seja possível eu calcular sua idade:"))
+def calculo_idade(i):
+    print(2025-i)
+    
+calculo_idade(idade)
+
+
+
+
+def mensagem():
+    print("Este é apenas um teste, seja bem vindo!!")
+
+novo = input("Qual o seu nome? ")
+
+def saudacao(i):
+    print("Saudação nobre",i)
+
+saudacao(novo)
