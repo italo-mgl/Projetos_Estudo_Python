4941ddca95906b92744867a2b0b462a1223e26d9
Itin <thenewera07@gmail.com>
2025-08-02 12:37:51 -0300

    salvando_dados

--- 13_API.py
+++ 13_API.py
@@ -1,9 +1,25 @@
 import requests
+import json
+from tqdm import tqdm
+import pandas as pd
+
+
 ceps = ["60540121", "60415580", "60540255"]
 
 url = "https://viacep.com.br/ws/{cep}/json/"
 
-response = requests.get(url)
-dados = response.json()
+dados = []
 
-print(dados)
+for i in tqdm(ceps):
+    response = requests.get(url.format(cep=i))
+    if response.status_code == 200:
+        dados.append(response.json())
+
+
+dataset = pd.DataFrame(dados)
+dataset.to_csv("ceps.csv", sep=";" )
+
+with open("ceps.json", "w", encoding='utf-8') as open_file:
+    json.dump(dados, open_file, ensure_ascii=False, indent=4)
+
+
